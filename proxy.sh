72a8c744b53fd2129d9f062af4b7d4f2cc75f692
Eugene Lee <elee@adelie.local>
2013-08-29 09:10:45 -0400

    Generated said SSH command.

--- proxy.sh
+++ proxy.sh
@@ -7,4 +7,5 @@ SSH_PORT=22                 # Port that you will be connecting to on the server.
 SSH_USER="elee"             # Username to access said server.
 SSH_HOST="orca.elee.me"     # The server you will be connecting to.
 INTERFACE="Wi-Fi"           # Interface that is reconfigured: run `networksetup -listallnetworkservices` to figure this out.
-
+# Initiation of SSH command to be used (Implementation of Mosh to be done soon).
+SSH_CMD="ssh ${SSH_OPTIONS} ${PORT} -p ${SSH_PORT} ${SSH_USER}@${SSH_HOST}"
