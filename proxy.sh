65c7f12dbf315f21b6b8954dbb242990eac83859
Eugene Lee <eugenelee@users.noreply.github.com>
2013-10-29 08:10:11 -0400

    Updated proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -4,7 +4,7 @@
 # Settings
 SSH_OPTIONS="-C2qTnND"      # SSH settings.
 PORT=8080                   # Localhost port - to be honest this doesn't matter, as we will seamlessly tell Networks/System Preferences this information.
-SSH_PORT=22                 # Port that you will be connecting to on the server.
+SSH_PORT=13370                 # Port that you will be connecting to on the server.
 SSH_USER="elee"             # Username to access said server.
 SSH_HOST="orca.elee.me"     # The server you will be connecting to.
 INTERFACE="Wi-Fi"           # Interface that is reconfigured: run `networksetup -listallnetworkservices` to figure this out.
